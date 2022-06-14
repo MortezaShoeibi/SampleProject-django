352f0589f7d17c98b95c14b3e96be338c493597a
ASUS <mortezashoeibi77@gmail.com>
2022-06-14 19:33:51 +0430

    creating the home view and fixing the project urls

--- AliShoeibi/urls.py
+++ AliShoeibi/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
