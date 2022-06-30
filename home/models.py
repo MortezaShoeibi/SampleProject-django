880a8b9e83a3eff4750ae27790c2eb991b5e2fb4
ASUS <mortezashoeibi77@gmail.com>
2022-06-30 15:25:22 +0430

    creating the ContactUs model

--- home/models.py
+++ home/models.py
@@ -81,3 +81,17 @@ class Video(models.Model):
     class Meta:
         verbose_name = 'ویدیو'
         verbose_name_plural = 'ویدیو'
+
+
+class ContactUs(models.Model):
+    name = models.CharField(max_length=100)
+    text = models.TextField()
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'پیام'
+        verbose_name_plural = 'پیام ها'
